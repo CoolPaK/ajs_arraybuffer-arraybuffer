f7e79386be6220f0c3c07c24128e9f1f5358b3e3
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-10-09 15:59:52 +0300

    cheange file

--- src/__test__/array-buffer-converter.test.js
+++ src/__test__/array-buffer-converter.test.js
@@ -7,22 +7,22 @@ describe('ArrayBufferConverter', () => {
             const buffer = getBuffer();
             converter.load(buffer);
             expect(converter.buffer).toBe(buffer);
-        
-    });
-    
+
+      });
+
       it('should throw error if buffer is not loaded', () => {
             const converter = new ArrayBufferConverter();
             expect(() => converter.toString()).toThrowError('Buffer is not loaded');
-        
-    });
-    
+
+      });
+
       it('should convert buffer to string correctly', () => {
             const converter = new ArrayBufferConverter();
             const buffer = getBuffer();
             converter.load(buffer);
             const str = converter.toString();
             expect(str).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
-        
-    });
-    
+
+      });
+
 });
--- src/js/Array-buffer-converter.js
+++ src/js/Array-buffer-converter.js
@@ -1,29 +1,29 @@
 class ArrayBufferConverter {
       constructor() {
             this.buffer = null;
-        
-    }
-    
+
+      }
+
       load(buffer) {
             this.buffer = buffer;
-        
-    }
-    
+
+      }
+
       toString() {
             if (!this.buffer) {
                   throw new Error('Buffer is not loaded');
-            
-        }
+
+            }
             const bufferView = new Uint16Array(this.buffer);
             let str = '';
             for (let i = 0; i < bufferView.length; i++) {
                   str += String.fromCharCode(bufferView[i]);
-            
-        }
+
+            }
             return str;
-        
-    }
-    
+
+      }
+
 }
 
 export default ArrayBufferConverter;
