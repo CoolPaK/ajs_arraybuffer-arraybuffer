f58cab28dd405330a372d4ff2f9626d59b377c3e
Oleg Valerevich <st-oleg58armia@mail.ru>
2024-10-10 14:27:59 +0300

    cheange file

--- src/js/Array-buffer-converter.js
+++ src/js/Array-buffer-converter.js
@@ -1,26 +1,26 @@
 class ArrayBufferConverter {
       constructor() {
-            this.buffer = null;
+          this.buffer = null;
 
       }
 
       load(buffer) {
-            this.buffer = buffer;
+          this.buffer = buffer;
 
       }
 
       toString() {
-            if (!this.buffer) {
-                  throw new Error('Buffer is not loaded');
+          if (!this.buffer) {
+              throw new Error('Buffer is not loaded');
 
-            }
-            const bufferView = new Uint16Array(this.buffer);
-            let str = '';
-            for (let i = 0; i < bufferView.length; i++) {
+          }
+          const bufferView = new Uint16Array(this.buffer);
+          let str = '';
+          for (let i = 0; i < bufferView.length; i++) {
                   str += String.fromCharCode(bufferView[i]);
 
-            }
-            return str;
+          }
+          return str;
 
       }
 
